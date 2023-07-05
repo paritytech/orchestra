b21fec24f7a33dd9f018abb7054078217ee31db5
Bernhard Schuster <bernhard@ahoi.io>
2023-07-05 11:57:26 +0200

    feat/lint: enforce dependency related lints
    Signed-off-by: Bernhard Schuster <bernhard@ahoi.io>

--- orchestra/proc-macro/src/lib.rs
+++ orchestra/proc-macro/src/lib.rs
@@ -13,6 +13,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #![allow(clippy::all)]
+#![deny(unused_crate_dependencies)]
+#![deny(unused_imports)]
+#![deny(unused_import_braces)]
 
 use proc_macro2::{Ident, Span, TokenStream};
 use syn::{parse_quote, spanned::Spanned, Path};
