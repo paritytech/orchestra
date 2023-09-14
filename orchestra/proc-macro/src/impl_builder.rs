4a90a790bb54bc8ff7e4d304aa53c3f007b519c8
Andrei Sandu <andrei-mihail@parity.io>
2023-09-14 18:41:27 +0300

    Support inject message via unbounded channels
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- orchestra/proc-macro/src/impl_builder.rs
+++ orchestra/proc-macro/src/impl_builder.rs
@@ -655,7 +655,7 @@ pub(crate) fn impl_feature_gated_items(
 							#channel_name: #channel_name_tx .clone(),
 						)*
 						#(
-							#channel_name_unbounded: #channel_name_unbounded_tx,
+							#channel_name_unbounded: #channel_name_unbounded_tx .clone(),
 						)*
 					};
 
@@ -694,6 +694,7 @@ pub(crate) fn impl_feature_gated_items(
 
 					let #subsystem_name: OrchestratedSubsystem< #consumes > =	spawn::<_,_, #blocking, _, _, _>(
 							&mut spawner,
+							#channel_name_unbounded_tx,
 							#channel_name_tx,
 							signal_tx,
 							unbounded_meter,
@@ -776,6 +777,7 @@ pub(crate) fn impl_task_kind(info: &OrchestraInfo) -> proc_macro2::TokenStream {
 		#[allow(clippy::too_many_arguments)]
 		pub fn spawn<S, M, TK, Ctx, E, SubSys>(
 			spawner: &mut S,
+			unbounded_message_tx: #support_crate ::metered::UnboundedMeteredSender<MessagePacket<#maybe_boxed_message_generic>>,
 			message_tx: #support_crate ::metered::MeteredSender<MessagePacket<#maybe_boxed_message_generic>>,
 			signal_tx: #support_crate ::metered::MeteredSender< #signal >,
 			// meter for the unbounded channel
@@ -832,6 +834,7 @@ pub(crate) fn impl_task_kind(info: &OrchestraInfo) -> proc_macro2::TokenStream {
 				},
 				tx_signal: signal_tx,
 				tx_bounded: message_tx,
+				tx_unbounded: unbounded_message_tx,
 				signals_received: 0,
 				name,
 			});
